d22d94188baeadc3b856023f088f8836b84c890e
Michael Bruce Allen <anchortone@Michaels-MBP.lan>
2024-09-13 15:18:29 -0400

    Add variables for easier changes

--- test-script.sh
+++ test-script.sh
@@ -3,20 +3,24 @@
 # Source the commands
 source src/commands.sh
 
+ver=1
+qrsize=15
+imageid=1
+
 # Initialize the printer
 init
 
 # Print some text
-text "Hello, ESC/POS Printer!"
+text "Print test: $ver"
 br 2
 
 # Print a QR code
-qrcode 2 5 "https://example.com"
-br 2
+# qrcode 2 $qrsize "https://example.com"
+# br 2
 
 # Print an image from memory by ID
-print_image_by_id 1
-br 2
+# print_image_by_id $imageid
+# br 2
 
 # Cut the paper
 cut
