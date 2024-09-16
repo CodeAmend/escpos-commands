28c3bc8cb0641fd5bb9afd6aca088e95ff84a5c2
Michael Bruce Allen <anchortone@Michaels-MBP.lan>
2024-09-16 14:51:03 -0400

    Change nv image upload to handle ascii chars

--- src/printer_utils/command_utils.py
+++ src/printer_utils/command_utils.py
@@ -1,20 +1,51 @@
 # printer_utils/command_utils.py
+def construct_store_command(image_data, image_id, width, height):
+    # Check that image_id is exactly two characters long
+    if len(image_id) != 2:
+        raise ValueError("image_id must be exactly two ASCII characters.")
 
-def construct_store_command(image_data, image_id):
     store_command = bytearray()
-    store_command.extend(b'\x1D\x28\x4C')  # GS ( L - Store NV Image command
-    store_command.extend([len(image_data) & 0xFF, (len(image_data) >> 8) & 0xFF])  # Data length
-    store_command.extend([image_id])  # Image ID
-    store_command.extend(image_data)  # Add the rasterized image data
+
+    # GS ( L
+    store_command.extend(b'\x1D\x28\x4C')
+
+    # Calculate pL and pH (Parameter Length)
+    # Data length = 10 bytes (parameters) + 1 byte ('c') + length of image_data
+    data_length = 10 + 1 + len(image_data)
+    pL = data_length & 0xFF
+    pH = (data_length >> 8) & 0xFF
+    store_command.extend([pL, pH])
+
+    # m and fn
+    store_command.extend(b'\x30\x43')  # m = 0x30 ('0'), fn = 0x43 ('C')
+
+    # a (Tone of data)
+    a = 0x30  # '0' for Monochrome
+    store_command.append(a)
+
+    # kc1 and kc2 (Key codes from image_id)
+    kc1 = ord(image_id[0])  # Convert first ASCII character to byte
+    kc2 = ord(image_id[1])  # Convert second ASCII character to byte
+    store_command.extend([kc1, kc2])
+
+    # b (Number of colors)
+    b = 0x01  # 1 for Monochrome
+    store_command.append(b)
+
+    # xL, xH (Width in pixels)
+    xL = width & 0xFF
+    xH = (width >> 8) & 0xFF
+    store_command.extend([xL, xH])
+
+    # yL, yH (Height in pixels)
+    yL = height & 0xFF
+    yH = (height >> 8) & 0xFF
+    store_command.extend([yL, yH])
+
+    # [c d1...dk]
+    c = 0x31  # '1' for Color 1 in Monochrome
+    store_command.append(c)
+    store_command.extend(image_data)
+
     return store_command
-
-def construct_delete_command(image_id):
-    delete_command = bytearray()
-    delete_command.extend(b'\x1D\x28\x4C\x04\x00\x30\x42')  # GS ( L - Delete command
-    delete_command.extend([image_id])  # Image ID
-    return delete_command
-
-def construct_check_memory_command():
-    check_command = b'\x1D\x69\x02'  # GS i 2 - Check memory status
-    return check_command
 
