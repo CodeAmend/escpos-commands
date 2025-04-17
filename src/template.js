3220a91056e0f16953248699457bda4e8dc38c74
Michael Bruce Allen <michael.bruceallen@IC-J3NHPL7D93.lan>
2025-04-17 09:58:52 -0400

    Update template with Page mode & QRcode

--- src/template.js
+++ src/template.js
@@ -1,25 +1,36 @@
 // template.js
 const escpos = require('./commands-node');
 
+const siteShort = "https://littlecaesars.com"
+
+const siteLong = "https://littlecaesars.com?orderId=1f2c9a6b-9824-4d63-a7d5-72b6323fbd1f&inventoryId=6a58c3d1-82b9-43d7-b241-2c93e44e2eb3&otherId=e5a3798a-bf56-4555-8702-313a4cb27263"
+
+const tx = 30;
+
 const buffer = Buffer.concat([
-  escpos.init(),
-  escpos.enterPageMode(),
-  escpos.definePageArea(0, 0, 576, 400),
+    escpos.init(),
 
-  escpos.setPosition(0, 50),
-  escpos.text('Top line'),
-  escpos.br(1),
+    escpos.enterPageMode(),
 
-  escpos.setPosition(0, 100),
-  escpos.text('Middle line'),
-  escpos.br(1),
+    escpos.definePageArea(0, 0, 576, 425),
 
-  escpos.setPosition(0, 150),
-  escpos.text('Bottom line'),
-  escpos.br(2),
+    escpos.setPosition(0, tx*1),
+    escpos.text('I want a'),
 
-  escpos.exitPageMode(),
-  escpos.cut()
+    escpos.setPosition(0, tx*2),
+    escpos.text('canvas image'),
+
+    escpos.setPosition(0, tx*3),
+    escpos.text('over here.'),
+
+    escpos.setPosition(315, 0),
+    escpos.qrcode(siteLong, 1, 4),
+
+    escpos.exitPageMode(),
+
+    escpos.br(4),
+
+    escpos.cut()
 ]);
 
 // Output binary to stdout (for piping to lp)
