b618dc11783c84cf09af4532d775fc006c1c5fd4
Michael Bruce Allen <michael.bruceallen@IC-J3NHPL7D93.lan>
2025-05-08 09:40:26 -0400

    New L90 Template

---
+++ src/l90.template-label.js
@@ -0,0 +1,56 @@
+// template.js
+const escpos = require('./commands-node');
+const { siteLong } = require('./values');
+
+(async () => {
+  // Convert man.png to raster data (16x29)
+
+  const buffer = Buffer.concat([
+    escpos.init(), // ESC @ (1B 40): Initialize
+    Buffer.from([0x1B, 0x74, 0x00]), // ESC t 0: PC437 code page
+
+    escpos.enterPageMode(),
+    escpos.setPrintArea(0, 0, 578, 330), 
+
+
+    escpos.selectFont(1), // Font B
+    escpos.setCharacterSize(2, 2), // 2x size
+    escpos.reverseBackground(true),
+    escpos.setJustification('left'),
+
+    escpos.setPosition(5, 60),
+    escpos.printText(" P "),
+    escpos.setCharacterSize(1, 1), // 2x size
+    escpos.reverseBackground(false),
+    escpos.printText(" Damon | #1004488 "),
+
+    escpos.setPosition(5, 130),
+    escpos.setCharacterSize(2, 2), // 2x size
+    escpos.reverseBackground(false),
+    escpos.printText("ST CR ULT SUPREME"),
+
+    escpos.setPosition(5, 175),
+    escpos.setCharacterSize(1, 1), // 2x size
+    escpos.printText("SAUCE: XX CHZ: XX PEP: XX IS: XX B: XX"),
+    escpos.setPosition(5, 215),
+    escpos.printText("SAUCE: XX CHZ: XX PEP: XX IS: XX B: XX"),
+
+    escpos.setPosition(5, 285),
+    escpos.setCharacterSize(2, 2), // 2x size
+    escpos.reverseBackground(true),
+    escpos.printText("  9  "),
+    escpos.reverseBackground(false),
+    escpos.printText("  9225  "),
+
+    escpos.setPosition(420, 0),
+    escpos.qrcode(siteLong, 2, 3),
+
+    escpos.printPageMode(),
+    escpos.cut(),
+
+    escpos.exitPageMode()
+  ]);
+
+  process.stdout.write(buffer); // Output to terminal
+})();
+
